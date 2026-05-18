05a3d7be6253446f23feb9d31b43a922c2fdf00b
Santiago <mesasantiago5@gmail.com>
2026-05-18 16:44:21 -0500

    test: reemplazar H2 por Testcontainers e integrar KafkaContainer

---
+++ src/test/java/com/registraduria/scrutiny_service/AbstractIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.registraduria.scrutiny_service;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("scrutiny_db")
+            .withUsername("test")
+            .withPassword("test");
+
+    @Container
+    public static KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0"));
+
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+    }
+}
--- src/test/java/com/registraduria/scrutiny_service/ScrutinyServiceApplicationTests.java
+++ src/test/java/com/registraduria/scrutiny_service/ScrutinyServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ScrutinyServiceApplicationTests {
+class ScrutinyServiceApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {

017723216ff5b9fd0e955b01afd0454609bffeb1
Santiago <mesasantiago5@gmail.com>
2026-05-27 16:15:35 -0500

    test: Estabilizar test de contexto con H2 y configurar CI con JDK 21

--- src/test/java/com/registraduria/scrutiny_service/ScrutinyServiceApplicationTests.java
+++ src/test/java/com/registraduria/scrutiny_service/ScrutinyServiceApplicationTests.java
@@ -3,7 +3,15 @@ package com.registraduria.scrutiny_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=PostgreSQL",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.flyway.enabled=false",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"server.port=0"
+})
 class ScrutinyServiceApplicationTests {
 
 	@Test
